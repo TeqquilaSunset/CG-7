90c9574e053235dfc5d695b08073dfb2fa7c8f84
Василий Быстрицкий <sc-2005@yandex.ru>
2022-11-22 17:39:09 +0700

    commit

--- CG-7/Form1.Designer.cs
+++ CG-7/Form1.Designer.cs
@@ -28,26 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.holst = new Tao.Platform.Windows.SimpleOpenGlControl();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.материалToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ambientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.diffuseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.specularToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.emissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.светToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.левоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.правоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.сВерхуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.offToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.xToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.yToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.zToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ambientTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.вариант1GreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // holst
@@ -61,184 +42,30 @@
             this.holst.ColorBits = ((byte)(32));
             this.holst.DepthBits = ((byte)(16));
             this.holst.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.holst.Location = new System.Drawing.Point(0, 24);
+            this.holst.Location = new System.Drawing.Point(0, 0);
+            this.holst.Margin = new System.Windows.Forms.Padding(4);
             this.holst.Name = "holst";
-            this.holst.Size = new System.Drawing.Size(634, 587);
+            this.holst.Size = new System.Drawing.Size(845, 752);
             this.holst.StencilBits = ((byte)(0));
             this.holst.TabIndex = 0;
             this.holst.KeyDown += new System.Windows.Forms.KeyEventHandler(this.holst_KeyDown);
             // 
-            // timer1
-            // 
-            this.timer1.Interval = 10;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.материалToolStripMenuItem,
-            this.светToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(634, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // материалToolStripMenuItem
-            // 
-            this.материалToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ambientToolStripMenuItem,
-            this.diffuseToolStripMenuItem,
-            this.specularToolStripMenuItem,
-            this.emissionToolStripMenuItem,
-            this.ambientTestToolStripMenuItem});
-            this.материалToolStripMenuItem.Name = "материалToolStripMenuItem";
-            this.материалToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
-            this.материалToolStripMenuItem.Text = "Материал";
-            // 
-            // ambientToolStripMenuItem
-            // 
-            this.ambientToolStripMenuItem.Name = "ambientToolStripMenuItem";
-            this.ambientToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.ambientToolStripMenuItem.Text = "Rubby";
-            this.ambientToolStripMenuItem.Click += new System.EventHandler(this.ambientToolStripMenuItem_Click);
-            // 
-            // diffuseToolStripMenuItem
-            // 
-            this.diffuseToolStripMenuItem.Name = "diffuseToolStripMenuItem";
-            this.diffuseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.diffuseToolStripMenuItem.Text = "Gold";
-            this.diffuseToolStripMenuItem.Click += new System.EventHandler(this.diffuseToolStripMenuItem_Click);
-            // 
-            // specularToolStripMenuItem
-            // 
-            this.specularToolStripMenuItem.Name = "specularToolStripMenuItem";
-            this.specularToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.specularToolStripMenuItem.Text = "Ambient off";
-            this.specularToolStripMenuItem.Click += new System.EventHandler(this.specularToolStripMenuItem_Click);
-            // 
-            // emissionToolStripMenuItem
-            // 
-            this.emissionToolStripMenuItem.Name = "emissionToolStripMenuItem";
-            this.emissionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.emissionToolStripMenuItem.Text = "Emission";
-            this.emissionToolStripMenuItem.Click += new System.EventHandler(this.emissionToolStripMenuItem_Click);
-            // 
-            // светToolStripMenuItem
-            // 
-            this.светToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.левоToolStripMenuItem,
-            this.правоToolStripMenuItem,
-            this.сВерхуToolStripMenuItem,
-            this.offToolStripMenuItem,
-            this.xToolStripMenuItem,
-            this.yToolStripMenuItem,
-            this.zToolStripMenuItem,
-            this.вариант1GreenToolStripMenuItem});
-            this.светToolStripMenuItem.Name = "светToolStripMenuItem";
-            this.светToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.светToolStripMenuItem.Text = "Свет";
-            // 
-            // левоToolStripMenuItem
-            // 
-            this.левоToolStripMenuItem.Name = "левоToolStripMenuItem";
-            this.левоToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.левоToolStripMenuItem.Text = "Вариант 1";
-            this.левоToolStripMenuItem.Click += new System.EventHandler(this.левоToolStripMenuItem_Click);
-            // 
-            // правоToolStripMenuItem
-            // 
-            this.правоToolStripMenuItem.Name = "правоToolStripMenuItem";
-            this.правоToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.правоToolStripMenuItem.Text = "Вариант 2";
-            this.правоToolStripMenuItem.Click += new System.EventHandler(this.правоToolStripMenuItem_Click);
-            // 
-            // сВерхуToolStripMenuItem
-            // 
-            this.сВерхуToolStripMenuItem.Name = "сВерхуToolStripMenuItem";
-            this.сВерхуToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.сВерхуToolStripMenuItem.Text = "С верху";
-            this.сВерхуToolStripMenuItem.Click += new System.EventHandler(this.сВерхуToolStripMenuItem_Click);
-            // 
-            // offToolStripMenuItem
-            // 
-            this.offToolStripMenuItem.Name = "offToolStripMenuItem";
-            this.offToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.offToolStripMenuItem.Text = "off";
-            this.offToolStripMenuItem.Click += new System.EventHandler(this.offToolStripMenuItem_Click);
-            // 
-            // xToolStripMenuItem
-            // 
-            this.xToolStripMenuItem.Name = "xToolStripMenuItem";
-            this.xToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.xToolStripMenuItem.Text = "X";
-            this.xToolStripMenuItem.Click += new System.EventHandler(this.xToolStripMenuItem_Click);
-            // 
-            // yToolStripMenuItem
-            // 
-            this.yToolStripMenuItem.Name = "yToolStripMenuItem";
-            this.yToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.yToolStripMenuItem.Text = "Y";
-            this.yToolStripMenuItem.Click += new System.EventHandler(this.yToolStripMenuItem_Click);
-            // 
-            // zToolStripMenuItem
-            // 
-            this.zToolStripMenuItem.Name = "zToolStripMenuItem";
-            this.zToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.zToolStripMenuItem.Text = "Z";
-            this.zToolStripMenuItem.Click += new System.EventHandler(this.zToolStripMenuItem_Click);
-            // 
-            // ambientTestToolStripMenuItem
-            // 
-            this.ambientTestToolStripMenuItem.Name = "ambientTestToolStripMenuItem";
-            this.ambientTestToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.ambientTestToolStripMenuItem.Text = "Ambient test";
-            this.ambientTestToolStripMenuItem.Click += new System.EventHandler(this.ambientTestToolStripMenuItem_Click);
-            // 
-            // вариант1GreenToolStripMenuItem
-            // 
-            this.вариант1GreenToolStripMenuItem.Name = "вариант1GreenToolStripMenuItem";
-            this.вариант1GreenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.вариант1GreenToolStripMenuItem.Text = "Вариант 1 green";
-            this.вариант1GreenToolStripMenuItem.Click += new System.EventHandler(this.вариант1GreenToolStripMenuItem_Click);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.ClientSize = new System.Drawing.Size(845, 752);
             this.Controls.Add(this.holst);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private Tao.Platform.Windows.SimpleOpenGlControl holst;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem материалToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ambientToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem diffuseToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem specularToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem emissionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem светToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem левоToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem правоToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem сВерхуToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem offToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem yToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem zToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ambientTestToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem вариант1GreenToolStripMenuItem;
     }
 }
 
--- CG-7/Form1.cs
+++ CG-7/Form1.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tao.OpenGl;
+using Tao.FreeGlut;
 
 namespace CG_7
 {
@@ -58,25 +59,26 @@ namespace CG_7
             //Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
             //Gl.glEnable(Gl.GL_BLEND);
 
-            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
-            Gl.glEnable(Gl.GL_DEPTH_TEST);
-            Gl.glEnable(Gl.GL_LIGHTING);
-            Gl.glEnable(Gl.GL_LIGHT0);
-            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
-            Gl.glEnable(Gl.GL_NORMALIZE);
-            
+            //Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+            //Gl.glEnable(Gl.GL_DEPTH_TEST);
+            //Gl.glEnable(Gl.GL_LIGHTING);
+            //Gl.glEnable(Gl.GL_LIGHT0);
+            //Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+            //Gl.glEnable(Gl.GL_NORMALIZE);
+
             //начальное и конечное значение для осей
             //Gl.glOrtho(-2, 2, -2, 2, 4, -50);
-
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB| Glut.GLUT_DOUBLE |Glut.GLUT_DEPTH);
             Gl.glMatrixMode(Gl.GL_PROJECTION);
             Gl.glLoadIdentity();
-            Gl.glFrustum(-2, 2, -2, 2, 2, 10);
+            Gl.glFrustum(-2, 2, -2, 2, 0.2f, 10);
 
             Gl.glMatrixMode(Gl.GL_MODELVIEW);
             Gl.glLoadIdentity();
 
             Gl.glViewport(0, 0, holst.Width, holst.Height);
-            Gl.glClearColor(0, 0, 0, 1);
+            Gl.glClearColor(1, 1, 1, 1);
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
 
             Draw();
@@ -108,123 +110,7 @@ namespace CG_7
 
         private void DrawLight()
         {
-            //фоновая подсветка
-            //Gl.glShadeModel(Gl.GL_SMOOTH); //затенение
-            float[] ambient = { 0.2f, 0.2f, 0.2f, 1 };
-            Gl.glLightModelfv(Gl.GL_LIGHT_MODEL_AMBIENT, ambient);
-            Gl.glEnable(Gl.GL_LIGHT0);
-            float[] light0_col = { 1, 1, 1, 1 };
-            Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, light0_col);
-            //float[] light_position = { 1.0f, 1.0f, 2.0f, 0.0f };
-            //Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_position);
-            //float[] light0_dif = { 1, 1, 1, 1};
-            //Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_AMBIENT, light0_dif
-
-            //float[] ambientlight = { 0.1f, 0.1f, 0.1f, 1.0f 
-
-            //float[] light0_dif = { 0f, 0f, 0f, 1 };
-            //float[] ambientlight = { 0.1f, 0.1f, 0.1f, 1.0f };
-            //Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_AMBIENT, ambientlight);
-            if ( l == 0)
-            {
-                Gl.glDisable(Gl.GL_LIGHT0);
-            }
-            if (l == 3)
-            {
-                //Gl.glRotatef(-60, 1, 0, 0);
-                //Gl.glRotatef(33, 0, 0, 1);
-                //Gl.glTranslatef(0, 0, -4);
-                Gl.glPushMatrix();
-                //Gl.glRotatef(30, 0, 0, 1);
-                //Gl.glRotatef(anglLight, 0, 1, 0);
-                float[] light0_pos = { 0, 2, 1, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                Gl.glTranslatef(0, 2, 1);
-                Gl.glRotatef(15, 0, 0, 1);
-                DrawQuad();
-                Gl.glPopMatrix();
-
-            }
-            else if (l == 1)
-            {
-                Gl.glRotatef(-60, 1, 0, 0);
-                Gl.glRotatef(33, 0, 0, 1);
-                Gl.glTranslatef(2, 3, -3);
-                Gl.glPushMatrix();
-                //Gl.glRotatef(30, 0, 0, 1);
-                Gl.glRotatef(anglLight, 0, 1, 0);
-                float[] light0_pos = { 0, 0, 1, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                Gl.glTranslatef(0, 0, 3);
-                Gl.glScalef(0.4f, 0.4f, 0.4f);
-                Gl.glColor3f(0, 0, 0);
-                DrawQuad();
-                Gl.glPopMatrix();
-            }
-            else if (l == 2)
-            {
-                Gl.glRotatef(-30, 0, 0, 1);
-                Gl.glRotatef(-60, 1, 0, 0);
-                Gl.glRotatef(33, 0, 0, 1);
-                Gl.glTranslatef(2, 3, -2);
-                Gl.glPushMatrix();
-                Gl.glRotatef(anglLight, 1, 0, 0);
-                float[] light0_pos = { 0, 0f, 2f, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                Gl.glTranslatef(0, 0, 2.5f);
-                Gl.glScalef(0.4f, 0.4f, 0.4f);
-                Gl.glColor3f(0, 0, 0);
-                DrawQuad();
-                Gl.glPopMatrix();
-            }
-            else if (l == 4)
-            {
-                float[] light0_pos = { 0, 0f, 4f, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-            }
-            else if (l == 5)
-            {
-                Gl.glPushMatrix();
-                float[] light0_pos = { 1, 0f, 0, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                //DrawQuad();
-                Gl.glPopMatrix();
-            }
-            else if (l == 6)
-            {
-                Gl.glPushMatrix();
-                float[] light0_pos = { 0, 1f, 0, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                //DrawQuad();
-                Gl.glPopMatrix();
-            }
-            else if (l == 7)
-            {
-                Gl.glPushMatrix();
-                float[] light0_pos = { 0, 0f, 1, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                //DrawQuad();
-                Gl.glPopMatrix();
-            }
-            else if (l == 8)
-            {
-                float[] light0_col2 = {0, 1, 0, 1 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, light0_col2);
-
-                Gl.glRotatef(-60, 1, 0, 0);
-                Gl.glRotatef(33, 0, 0, 1);
-                Gl.glTranslatef(2, 3, -3);
-                Gl.glPushMatrix();
-                //Gl.glRotatef(30, 0, 0, 1);
-                Gl.glRotatef(anglLight, 0, 1, 0);
-                float[] light0_pos = { 0, 0, 1, 0 };
-                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light0_pos);
-                Gl.glTranslatef(0, 0, 3);
-                Gl.glScalef(0.4f, 0.4f, 0.4f);
-                Gl.glColor3f(0, 0, 0);
-                DrawQuad();
-                Gl.glPopMatrix();
-            }
+           
 
         }
         private void DrawQuad()
@@ -250,97 +136,7 @@ namespace CG_7
             // характеристики материалов
             //http://devernay.free.fr/cours/opengl/materials.html
             //rubby 
-            if (s == 1)
-            {
-                //float[] color_am = { 0.1747f, 0.01175f, 0.01175f, 1.0f };
-                //float[] color_dif = { 0.61424f, 0.04136f, 0.04136f };
-                //float[] color_spec = { 0.727811f, 0.626959f, 0.0626959f };
-                //float[] emission = {0.0f, 0.0f, 0.0f, 1 };
-                //float color_shin = 0.6f;
-
-                float[] color_am = { 0.1747f, 0.01175f, 0.01175f, 1.0f };
-                float[] color_dif = { 0.61424f, 0.04136f, 0.04136f };
-                float[] color_spec = { 0.727811f, 0.626959f, 0.0626959f };
-                float[] emission = { 0.0f, 0.0f, 0.0f, 1 };
-                float color_shin = 0.1f;
-
-                Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, color_am);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, color_dif);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, color_spec);
-                Gl.glMaterialf(Gl.GL_FRONT_AND_BACK, Gl.GL_SHININESS, color_shin * 128.0f);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, emission);
-            }
-            //gold
-            if (s == 2)
-            {
-                float[] color_am = { 0.24725f, 0.1995f, 0.0745f, 1.0f };
-                float[] color_dif = { 0.75164f, 0.60648f, 0.22648f };
-                float[] color_spec = { 0.628281f, 0.555802f, 0.366065f };
-                float[] emission = { 0.0f, 0.0f, 0.0f, 1 };
-                float color_shin = 0.4f;
-
-                Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, color_am);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, color_dif);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, color_spec);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, emission);
-                Gl.glMaterialf(Gl.GL_FRONT_AND_BACK, Gl.GL_SHININESS, color_shin * 128.0f);
-            }
-            //specular
-            if (s == 3)
-            {
-                //float[] color_am = { 0.19225f, 0.19225f, 0.19225f, 1.0f };
-                //float[] color_dif = { 0.50754f, 0.50754f, 0.50754f };
-                //float[] color_spec = { 0.508273f, 0.508273f, 0.508273f };
-                //float[] emission = { 0.0f, 0.0f, 0.0f, 1 };
-                //float color_shin = 0.4f;
-
-                float[] color_am = { 0.0f, 0.0f, 0.0f, 1.0f };
-                float[] color_dif = { 0.76f, 0.68f, 0.88f };
-                float[] color_spec = { 0.0f, 0.0f, 0.0f };
-                float[] emission = { 0.0f, 0.0f, 0.0f, 1 };
-                float color_shin = 0.4f;
-
-                Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, color_am);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, color_dif);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, color_spec);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, emission);
-                Gl.glMaterialf(Gl.GL_FRONT_AND_BACK, Gl.GL_SHININESS, color_shin * 128.0f);
-            }
-            //emission
-            if (s == 4)
-            {
-                //float[] color_am = { 0.0f, 0.05f, 0.0f, 1.0f };
-                //float[] color_dif = { 0.4f, 0.5f, 0.4f };
-                //float[] color_spec = { 0.04f, 0.7f, 0.04f };
-                //float[] emission = { 0.3f, 0.75f, 0.2f, 1 };
-                //float color_shin = 0.78125f;
-
-                //Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-                //Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, color_am);
-                //Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, color_dif);
-                //Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, color_spec);
-                //Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, emission);
-                //Gl.glMaterialf(Gl.GL_FRONT_AND_BACK, Gl.GL_SHININESS, color_shin * 128.0f);
-
-                Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-                float[] emission = { 0, 1, 0, 0 };
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, new float[] { .2f, .2f, .2f, 1 });
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, new float[] { .8f, .8f, .8f, 1 });
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, new float[] { 0, 0, 0, 1 });
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, emission);
-            }
-            if(s == 5)
-            {
-                Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-                float[] amb = { 1, 0, 0, 0 };
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, amb);
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, new float[] { .8f, .8f, .8f, 1 });
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, new float[] { 0, 0, 0, 1 });
-                Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, new float[] { 0, 0, 0, 1 });
-            }
+            
 
 
             //////////////////////
@@ -348,22 +144,9 @@ namespace CG_7
             Gl.glRotatef(Y, 0, 1, 0);
             Gl.glRotatef(Z, 0, 0, 1);
             //Gl.glColor3f(0.8f, 0.2f, 0.2f);
-            Gl.glNormalPointer(Gl.GL_FLOAT, 0, normal);
-
-            Gl.glDrawElements(Gl.GL_QUADS, 24, Gl.GL_UNSIGNED_INT, indexQuads);
-            Gl.glDrawElements(Gl.GL_POLYGON, 6, Gl.GL_UNSIGNED_INT, indexPolygon);
-            Gl.glDrawElements(Gl.GL_POLYGON, 6, Gl.GL_UNSIGNED_INT, indexPolygon2);
-
-            Gl.glColor3f(0, 0, 0);
-            Gl.glLineWidth(1);
-            Gl.glDrawElements(Gl.GL_LINES, 36, Gl.GL_UNSIGNED_INT, indexLines);
-            Gl.glTranslated(-0.01, 0.01, 0);
-            Gl.glDrawElements(Gl.GL_LINES, 2, Gl.GL_UNSIGNED_INT, indexLinesLoop);
-
-            //////////////////////////
-            //Gl.glDisable(Gl.GL_COLOR_MATERIAL);
-            Gl.glDisableClientState(Gl.GL_VERTEX_ARRAY);
-            Gl.glDisableClientState(Gl.GL_NORMAL_ARRAY);
+            
+            Gl.glColor3f(1,1,1);
+            Glut.glutSolidSphere(0.5, 20, 20);
 
             holst.Invalidate();
         }
@@ -419,108 +202,15 @@ namespace CG_7
                 if (flag == true)
                 {
                     flag = false;
-                    timer1.Stop();
+                    //timer1.Stop();
                 }
                 else if (flag == false)
                 {
                     flag = true;
-                    timer1.Start();
+                    //timer1.Start();
                 }
             }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            //anglc++;
-            anglLight += 1f;
-            Draw();
-        }
-        
-        private void левоToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 1;
-            Draw();
-        }
-
-        private void правоToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 2;
-            Draw();
-        }
-
-        private void сВерхуToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 3;
-            Draw();
-        }
-
-        private void сПередиToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ambientToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            s = 1;
-            Draw();
-        }
-
-        private void diffuseToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            s = 2;
-            Draw();
-        }
-
-        private void specularToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            s = 3;
-            Draw();
-        }
-
-        private void emissionToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            s = 4;
-            Draw();
-        }
-
-        private void offToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 0;
-        }
-
-        private void правоToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void xToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 5;
-            Draw();
-        }
-
-        private void yToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 6;
-            Draw();
-        }
-
-        private void zToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 7;
-            Draw();
-        }
-
-        private void ambientTestToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            s = 5;
-            Draw();
-        }
-
-        private void вариант1GreenToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            l = 8;
-            Draw();
-        }
     }
 }
